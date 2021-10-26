da827f601f50a490677ba36e580bc269975e495a
juanjof1 <salazargiljuanjose@gmail.com>
2021-10-25 21:12:03 -0600

    react Change

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { Calculator } from "./Calculator/Calculator";
-import { Link } from "react-router-dom";
 import axios from "axios";
 export const About = () => {
   const API_URL_UPCOMING =
